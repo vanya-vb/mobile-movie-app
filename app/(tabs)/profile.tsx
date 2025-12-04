b3ed4dbf3579613a33a5837c16d4c60365c67db8
vanya-vb <vbozadzhieva29@gmail.com>
2025-12-04 17:50:27 +0200

    add navigation and routing

---
+++ app/(tabs)/profile.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function Profile() {
+    return (
+        <View
+            className="flex-1 justify-center items-center"
+        >
+            <Text className="text-5xl text-dark-100 font-bold">Profile</Text>
+        </View>
+    );
+}
--- app/(tabs)/saved.tsx
+++ app/(tabs)/saved.tsx
@@ -1,11 +1,11 @@
 import { Text, View } from "react-native";
 
-export default function Index() {
+export default function Saved() {
     return (
         <View
             className="flex-1 justify-center items-center"
         >
-            <Text className="text-5xl text-dark-100 font-bold">Welcome!!</Text>
+            <Text className="text-5xl text-dark-100 font-bold">Saved</Text>
         </View>
     );
 }
